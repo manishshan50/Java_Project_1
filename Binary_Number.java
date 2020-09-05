10322d38e11e1831eb94921a2e647cbfad016106
Manish <63234458+manishshan50@users.noreply.github.com>
2020-09-05 13:01:34 +0530

    Update Binary_Number.java

--- Binary_Number.java
+++ Binary_Number.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 public class Solution {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
@@ -7,7 +6,6 @@ public class Solution {
         scan.close();
         System.out.println(consecutiveOnes(n));
     }
-    
     public static int consecutiveOnes(int n) {
         int count = 0;
         int max   = 0;
